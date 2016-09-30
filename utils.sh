9540a07c826a6b8f1639c0e258a9386ea6117107
Akuma Huang <ihuangwj@gmail.com>
2016-09-30 23:15:46 +0800

    添加包含工具函数的 shell 脚步

---
+++ utils.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+# A shell script about some util functions.
+
+hash_hmac() {
+  local data="$1"
+  local key="$2&"
+  echo -n "$data" | openssl sha1 -binary -hmac "$key" | base64
+}
+
+join_by() {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
+
+url_encode() {
+  local str="$1"
+  local strlen=${#str}
+  local encoded=""
+  local pos c o
+
+  for (( pos = 0 ; pos < strlen ; pos++ )); do
+    c=${str:$pos:1}
+    case "$c" in
+      [-_.~a-zA-Z0-9] )
+        o="$c"
+        ;;
+      * )
+        printf -v o '%%%02X' "'$c"
+    esac
+    encoded+="$o"
+  done
+  echo "$encoded"
+}
+
+random_char() {
+  local chars="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  local i=$(( $RANDOM % ${#chars} ))
+  echo -n "${chars:$i:1}"
+}
+
+random_chars() {
+  local len="$1"
+  for i in $(seq 1 $len); do echo -n $(random_char); done
+}
+
+git_pull_need() {
+  if [ git_pull_check = 'Need to pull' ]; then
+    echo true
+  else
+    echo false
+  fi
+}
+
+git_pull_check() {
+  git remote update
+
+  local UPSTREAM="${1:-'@{u}'}"
+  local LOCAL=$(git rev-parse @)
+  local REMOTE=$(git rev-parse "$UPSTREAM")
+  local BASE=$(git merge-base @ "$UPSTREAM")
+
+  if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+  elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+  elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
+  else
+    echo "Diverged"
+  fi
+}
+
+debug() {
+  if [[ $DEBUG = true ]]; then
+    local params=$*
+    echo
+    echo "$params"
+    echo
+  fi
+}
