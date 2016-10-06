5dd8c6d34766a3308e85c4dd110620dc7c416270
Akuma Huang <ihuangwj@gmail.com>
2016-10-06 20:49:47 +0800

    添加控制阿里云 slb 的脚本

--- gam.sh
+++ gam.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # GAM is a Guomi App Manager tool.
 
 REPOS_ROOT=/opt/app-deploy
@@ -9,276 +9,318 @@ REPOS_PAGES=$REPOS_ROOT/clearn-pages
 REPOS_STUDENT=$REPOS_ROOT/clearn-student
 REPOS_TEACHER=$REPOS_ROOT/clearn-teacher
 REPOS_SUPPORT=$REPOS_ROOT/clearn-support
-REPOS_ASK=$REPOS_ROOT/clearn-ask
 
-TOMCAT_SUPPORT_KEY=tomcat-support
-TOMCAT_SUPPORT_HOME=/opt/tomcat/$TOMCAT_SUPPORT_KEY
 TOMCAT_WEB_HOME=/opt/tomcat7
 CATALINA_LOG=logs/catalina.out
 
+# Source slb functions
+current_dir=$(dirname "$0")
+# shellcheck source=src/slb.sh
+. "$current_dir/slb.sh"
+
 print_title() {
-    echo
-    echo "============================================================"
-    echo $1
+  echo
+  echo "============================================================"
+  echo "$1"
 }
 
 print_footer() {
-    echo $1
-    echo "============================================================"
+  echo "$1"
+  echo "============================================================"
 }
 
 # 更新静态资源
 update_assets() {
-    print_title "开始更新 clearn-assets ..."
+  print_title "开始更新 clearn-assets ..."
 
-    app_repos="$REPOS_ASSETS $REPOS_STATIC"
-    for app in $app_repos
-    do
-        cd $app
-        git checkout .
-        git pull
-    done
+  app_repos=( "$REPOS_ASSETS" "$REPOS_STATIC" )
+  for app in "${app_repos[@]}"
+  do
+    cd "$app" || return
+    if [ "$(git_pull_need)" = true ]; then
+      git checkout .
+      git pull
+    fi
+  done
 
-    cd $REPOS_ASSETS
+  cd "$REPOS_ASSETS" || return
+  if [ "$(git_pull_need)" = true ]; then
     grunt publish
+  fi
 
-    cd $REPOS_STATIC
+  cd "$REPOS_STATIC" || return
+  if [ "$(git_pull_need)" = true ]; then
     grunt publish
+  fi
 
-    print_footer "clearn-assets 更新完毕"
+  print_footer "clearn-assets 更新完毕"
 }
 
 # 更新静态资源 v2
 update_assets2() {
-    print_title "开始更新 clearn-assets2 ..."
+  print_title "开始更新 clearn-assets2 ..."
 
-    cd $REPOS_ASSETS2
+  cd "$REPOS_ASSETS2" || return
+  if [ "$(git_pull_need)" = true ]; then
     git checkout .
     git pull
     gulp dist
+  fi
 
-    print_footer "clearn-assets2 更新完毕"
+  print_footer "clearn-assets2 更新完毕"
 }
 
 # 更新运营平台
 update_support() {
-    print_title "开始更新 web-03 ..."
-    app_repos="$REPOS_PAGES $REPOS_SUPPORT"
-    ssh -t dev@web-03 "$( typeset -f ); update_web_apps $app_repos"
-    print_footer "web-03 更新完毕"
+  print_title "开始更新 web-03 ..."
+  app_repos=( "$REPOS_PAGES" "$REPOS_SUPPORT" )
+  ssh -t dev@web-03 "$(typeset -f); update_web_apps" "${app_repos[@]}"
+  print_footer "web-03 更新完毕"
 }
 
 # 更新 web 服务器程序主函数
 update_web() {
-    print_title "开始更新 web-$1 ..."
-    app_repos="$REPOS_PAGES $REPOS_STUDENT $REPOS_TEACHER"
-    ssh -t dev@web-$1 "$( typeset -f ); update_web_apps $app_repos"
-    print_footer "web-$1 更新完毕"
+  print_title "开始更新 web-$1 ..."
+  app_repos=( "$REPOS_PAGES" "$REPOS_STUDENT" "$REPOS_TEACHER" )
+  ssh -t dev@web-"$1" "$(typeset -f); update_web_apps" "${app_repos[@]}"
+  print_footer "web-$1 更新完毕"
 }
 
 # 更新 web 服务器上的应用程序
 update_web_apps() {
-    #app_repos=$*
-    app_repos=( $* )
-    for app in $app_repos
-    do
-        echo "---------------------------------------------"
-        echo "开始更新 ${app:15} ..."
-        cd $app
-        git checkout .
-        git pull
-        echo "${app:15} 更新完毕"
-        echo
-    done
+  app_repos=( "$@" )
+  echo "${app_repos[@]}"
+  for app in "${app_repos[@]}"
+  do
+    echo "---------------------------------------------"
+    echo "开始更新 ${app:15} ..."
+    cd "$app" || return
+
+    if [ "$(git_pull_need)" = true ]; then
+      git checkout .
+      git pull
+    fi
+
+    echo "${app:15} 更新完毕"
+    echo
+  done
 }
 
 update() {
-    case "$1" in
-        "assets" )
-            update_assets
-            update_assets2
-        ;;
-
-        "assets2" )
-            update_assets2
-        ;;
-
-        "support" )
-            update_support
-        ;;
-
-        "web01" )
-            update_web "01"
-        ;;
-
-        "web02" )
-            update_web "02"
-        ;;
-
-        * )
-            echo "Unknow update target, please choose again: $0 update [target]"
-    esac
+  case "$1" in
+    "assets" )
+      update_assets
+      update_assets2
+      ;;
+    "assets2" )
+      update_assets2
+      ;;
+    "support" )
+      update_support
+      ;;
+    "web01" )
+      update_web "01"
+      ;;
+    "web02" )
+      update_web "02"
+      ;;
+    * )
+      echo "Unknow update target, please choose again: $0 update <server>"
+  esac
 }
 
 start() {
-    case "$1" in
-        "web01" )
-            start_web "01"
-        ;;
-
-        "web02" )
-            start_web "02"
-        ;;
-
-        "support" )
-            start_web "03"
-        ;;
-
-        * )
-            echo "Unknow start target, please choose again: $0 start [target]"
-    esac
+  case "$1" in
+    "web01" )
+      start_web "01"
+      ;;
+    "web02" )
+      start_web "02"
+      ;;
+    "support" )
+      start_web "03"
+      ;;
+    * )
+      echo "Unknow start target, please choose again: $0 start <server>"
+  esac
 }
 
 start_web() {
-    ssh -t dev@web-$1 "sudo systemctl start tomcat"
-    echo "web-$1 is starting..."
+  ssh -t dev@web-"$1" "sudo systemctl start tomcat"
+  echo "web-$1 is starting..."
 }
 
 stop() {
-    case "$1" in
-        "web01" )
-            stop_web "01"
-        ;;
-
-        "web02" )
-            stop_web "02"
-        ;;
-
-        "support" )
-            stop_web "03"
-        ;;
-
-        * )
-            echo "Unknow stop target, please choose again: $0 stop [target]"
-    esac
+  case "$1" in
+    "web01" )
+      stop_web "01"
+      ;;
+    "web02" )
+      stop_web "02"
+      ;;
+    "support" )
+      stop_web "03"
+      ;;
+    * )
+      echo "Unknow stop target, please choose again: $0 stop <server>"
+  esac
 }
 
 stop_web() {
-    ssh -t dev@web-$1 "sudo systemctl stop tomcat"
-    echo "web-$1 is stopping..."
+  ssh -t dev@web-"$1" "sudo systemctl stop tomcat"
+  echo "web-$1 is stopping..."
 }
 
 restart() {
-    case "$1" in
-        "web01" )
-            restart_web "01"
-        ;;
-
-        "web02" )
-            restart_web "02"
-        ;;
-
-        "support" )
-            restart_web "03"
-        ;;
-
-        * )
-            echo "Unknow restart target, please choose again: $0 restart [target]"
-    esac
+  case "$1" in
+    "web01" )
+      up_server "02"
+      down_server "01"
+      restart_web "01"
+      ;;
+    "web02" )
+      up_server "01"
+      down_server "02"
+      restart_web "02"
+      ;;
+    "support" )
+      restart_web "03"
+      ;;
+    * )
+      echo "Unknow restart target, please choose again: $0 restart <server>"
+  esac
 }
 
 restart_web() {
-    ssh -t dev@web-$1 "sudo systemctl restart tomcat"
-    echo "web-$1 is restarting..."
+  ssh -t dev@web-"$1" "sudo systemctl restart tomcat"
+  echo "web-$1 is restarting..."
+}
+
+up() {
+  case "$1" in
+    "web01" )
+      up_server "$ALIYUN_WEB01_ID"
+      ;;
+    "web02" )
+      up_server "$ALIYUN_WEB02_ID"
+      ;;
+    * )
+      echo "Unknow up target, please choose again: $0 up <server>"
+  esac
+}
+
+down() {
+  case "$1" in
+    "web01" )
+      down_server "$ALIYUN_WEB01_ID"
+      ;;
+    "web02" )
+      down_server "$ALIYUN_WEB02_ID"
+      ;;
+    * )
+      echo "Unknow down target, please choose again: $0 down <server>"
+  esac
+}
+
+up_server() {
+  local serverId="$1"
+  set_backend_servers "$ALIYUN_SLB_ID" "$serverId" "100"
+}
+
+down_server() {
+  local serverId="$1"
+  set_backend_servers "$ALIYUN_SLB_ID" "$serverId" "0"
 }
 
 status() {
-    case "$1" in
-        "web01" )
-            status_web "01"
-        ;;
-
-        "web02" )
-            status_web "02"
-        ;;
-
-        "support" )
-            status_web "03"
-        ;;
-
-        * )
-           echo "Unknow status target, please choose again: $0 status [target]"
-    esac
+  case "$1" in
+    "web01" )
+      status_web "01"
+      ;;
+    "web02" )
+      status_web "02"
+      ;;
+    "support" )
+      status_web "03"
+      ;;
+    * )
+      echo "Unknow status target, please choose again: $0 status <server>"
+  esac
 }
 
 status_web() {
-    ssh -t dev@web-$1 "sudo systemctl status tomcat -l"
+  ssh -t dev@web-"$1" "sudo systemctl status tomcat -l"
 }
 
 log() {
-    case "$1" in
-        "web01" )
-            log_web "01"
-        ;;
-
-        "web02" )
-            log_web "02"
-        ;;
-
-        "support" )
-            log_web "03"
-        ;;
-
-        * )
-            echo "Unknow log target, please choose again: $0 log [target]"
-    esac
+  case "$1" in
+    "web01" )
+      log_web "01"
+      ;;
+    "web02" )
+      log_web "02"
+      ;;
+    "support" )
+      log_web "03"
+      ;;
+    * )
+      echo "Unknow log target, please choose again: $0 log <server>"
+  esac
 }
 
 log_web() {
-    ssh -t dev@web-$1 "tail -n 100 -f $TOMCAT_WEB_HOME/$CATALINA_LOG"
+  ssh -t dev@web-"$1" "tail -n 100 -f $TOMCAT_WEB_HOME/$CATALINA_LOG"
 }
 
 help() {
-    echo "Usage: $0 [OPTION] [TARGET]"
-    echo
-    echo "  upgrade [support|web**]         update app and restart app"
-    echo "  update  [assets|support|web**]  update app"
-    echo "  start   [support|web**]         start app"
-    echo "  stop    [support|web**]         stop app"
-    echo "  restart [support|web**]         restart app"
-    echo "  status  [support|web**]         show started app info"
-    echo "  log     [support|web**]         show last app logs"
-    echo "  help                            display this help and exit"
-    echo
+  echo "Usage: $0 <command>"
+  echo
+  echo "  upgrade <support|web**>         update app and restart app"
+  echo "  update  <assets|support|web**>  update app"
+  echo "  start   <support|web**>         start app"
+  echo "  stop    <support|web**>         stop app"
+  echo "  restart <support|web**>         restart app"
+  echo "  up      <web**>                 restart app"
+  echo "  down    <web**>                 restart app"
+  echo "  status  <support|web**>         show started app info"
+  echo "  log     <support|web**>         show last app logs"
+  echo "  help                            display this help and exit"
+  echo
 }
 
 RETVAL=0
 
 case "$1" in
-    update )
-        update $2
+  update )
+    update "$2"
     ;;
-    start )
-        start $2
+  start )
+    start "$2"
     ;;
-    stop )
-        stop $2
+  stop )
+    stop "$2"
     ;;
-    restart )
-        restart $2
+  restart )
+    restart "$2"
     ;;
-    status )
-        status $2
+  up )
+    up "$2"
     ;;
-    log )
-        log $2
+  down )
+    down "$2"
     ;;
-    upgrade )
-        update assets && update $2 && restart $2 && log $2
+  status )
+    status "$2"
     ;;
-    * )
-      help
-      RETVAL=1
+  log )
+    log "$2"
+    ;;
+  upgrade )
+    update assets && update "$2" && restart "$2" && log "$2"
+    ;;
+  * )
+    help
+    RETVAL=1
 esac
 
 exit $RETVAL
---
+++ slb.sh
@@ -0,0 +1,72 @@
+#!/usr/local/bin/bash
+# A shell script for managing AliYun SLB.
+
+# Check bash version
+if [ ${BASH_VERSINFO[0]} -lt 4 ]; then
+  echo "Sorry, you need at least bash-4.0 to run this script."
+  exit 1
+fi
+
+# Check aliyun access key id
+if [ -z $ALIYUN_ACCESS_KEY_ID ]; then
+  echo "Please set environment variable 'ALIYUN_ACCESS_KEY_ID' (AliYun Access Key ID)"
+  exit 1
+fi
+
+# Check aliyun access key secret
+if [ -z $ALIYUN_ACCESS_KEY_SECRET ]; then
+  echo "Please set environment variable 'ALIYUN_ACCESS_KEY_SECRET' (AliYun Access Key Secret)"
+  exit 1
+fi
+
+# Source utils functions
+current_dir=$(dirname "$0")
+# shellcheck source=src/utils.sh
+. "$current_dir/utils.sh"
+
+# shellcheck disable=2154,2155
+set_backend_servers() {
+  local slbId="$1"
+  local serverId="$2"
+  local serverWeight="$3"
+
+  declare -A params
+  params[AccessKeyId]="$ALIYUN_ACCESS_KEY_ID"
+  params[Format]="json"
+  params[Version]="2014-05-15"
+  params[Timestamp]=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+  params[SignatureVersion]="1.0"
+  params[SignatureMethod]="HMAC-SHA1"
+  params[SignatureNonce]=$(random_chars 16)
+
+  params[Action]="SetBackendServers"
+  params[LoadBalancerId]="$slbId"
+  params[BackendServers]="[{\"ServerId\":\"$serverId\",\"Weight\":\"$serverWeight\"}]"
+
+  local originParams=""
+  for k in "${!params[@]}"; do originParams+="$k=${params[$k]}&"; done
+  originParams="${originParams:0:${#originParams} - 1}"
+  debug "originParams: $originParams"
+
+  debug "paramNames:" "${!params[@]}"
+  local sortedParamNames=($(for p in "${!params[@]}"; do echo $p; done | sort))
+  debug "sortedParamNames:" "${sortedParamNames[@]}"
+
+  local sortedParams=""
+  for k in "${sortedParamNames[@]}"; do sortedParams+="$k=$(url_encode "${params[$k]}")&"; done
+  sortedParams="${sortedParams:0:${#sortedParams} - 1}"
+  debug "sortedParams: $sortedParams"
+
+  local encodedParams=$(url_encode $sortedParams)
+
+  local strToSign="GET&%2F&$encodedParams"
+  debug "strToSign: $strToSign"
+
+  local signature=$(url_encode "$(hash_hmac $strToSign $ALIYUN_ACCESS_KEY_SECRET)")
+
+  local finalParams="$originParams&Signature=$signature"
+  debug "finalParams: $finalParams"
+
+  curl -G "https://slb.aliyuncs.com/" -d "$finalParams"
+  echo
+}
